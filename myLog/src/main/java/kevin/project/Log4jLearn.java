2731323183d2f2e54ec5bbdb404d2fbe47c649c9
Kevin Wang <kevinwang09@foxmail.com>
2023-02-22 23:28:41 +0800

    fedora

--- myLog/src/main/java/kevin/project/Log4jLearn.java
+++ myLog/src/main/java/kevin/project/Log4jLearn.java
@@ -2,14 +2,15 @@ package kevin.project;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.Layout;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.Configuration;
 import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.core.layout.PatternLayout;
 
 public class Log4jLearn {
-    private static Logger logger = LogManager.getLogger();
 
     public void basicLog() {
         Logger logger1 = LogManager.getLogger();
@@ -17,20 +18,25 @@ public class Log4jLearn {
     }
 
     public void anotherConfig() {
-        Configurator.initialize(null, "log4j2_1.xml");
+        LoggerContext context = Configurator.initialize(null, "log4j2_1.xml");
         Logger logger2 = LogManager.getLogger();
         logger2.info("this is logger 2");
+        context.stop();
+
     }
 
     public void codeConfig() {
+        Configurator.reconfigure();
         String logPath = "application2.log";
         LoggerContext context = (LoggerContext) LogManager.getContext();
         Configuration config = context.getConfiguration();
+        Layout<?> layout = PatternLayout.newBuilder()
+                .withPattern("%d{yyyy-MM-dd HH:mm:ss.SSS} [%t] %-5level %logger{36} - %msg%n")
+                .build();
         FileAppender appender = FileAppender.newBuilder()
                 .withFileName(logPath)
-                .withName("MyAppender")
-                .withImmediateFlush(true)
-                .withIgnoreExceptions(false)
+                .setLayout(layout)
+                .setName("test")
                 .setConfiguration(config)
                 .build();
 
@@ -41,9 +47,9 @@ public class Log4jLearn {
 
         // Update the configuration
         Configurator.initialize(config);
-
+        Logger logger = LogManager.getLogger();
         // Log a message
-        logger.info("Hello, world!");
+        logger.error("Hello, world!");
 
         // Shutdown the LoggerContext
         context.stop();
