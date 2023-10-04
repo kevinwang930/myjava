e2387e93e8482ca158013787712d4cb30eefdd8d
Kevin Wang <kevinwang09@foxmail.com>
2023-10-05 00:23:11 +0800

    fedora push

---
+++ library/mySpring/src/main/java/kevin/project/servlet/MyServlet.java
@@ -0,0 +1,6 @@
+package kevin.project.servlet;
+
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class MyServlet extends SpringBootServletInitializer {
+}
