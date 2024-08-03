ec88a7439c336ba748e3e614628d882953ecc557
marlsen <marlsenwang@hotmail.com>
2024-08-03 19:23:28 +0800

    ky

--- syntax/src/main/java/kevin/project/lock/ObjectLockLearn.java
+++ syntax/src/main/java/kevin/project/lock/ObjectLockLearn.java
@@ -2,6 +2,7 @@ package kevin.project.lock;
 
 import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
+import org.openjdk.jol.info.ClassLayout;
 
 /**
  * @version 1.0
@@ -14,22 +15,26 @@ public class ObjectLockLearn {
 
     public static void objectLockLearn() throws InterruptedException {
         Object lock = new Object();
-
+        System.out.println(ClassLayout.parseInstance(lock).toPrintable());
         Thread thread = new Thread(() -> {
             try {
                 synchronized(lock) {
                     System.out.println("start wait");
+                    System.out.println(ClassLayout.parseInstance(lock).toPrintable());
                     lock.wait();
                     System.out.println("end wait");
+                    System.out.println(ClassLayout.parseInstance(lock).toPrintable());
                 }
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
         });
         thread.start();
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         synchronized (lock) {
+            System.out.println("start notify");
             lock.notify();
+            System.out.println("end notify");
         }
         thread.join();
     }
