355ad75664b6e5002c910d897fc335144e45cbee
marlse <marlse@itmail888.com>
2025-01-09 10:58:32 +0700

    db

--- library/encryption/src/main/java/kevin/project/encryption/Md5Learn.java
+++ library/encryption/src/main/java/kevin/project/encryption/Md5Learn.java
@@ -1,26 +1,41 @@
 package kevin.project.encryption;
 
+import cn.hutool.crypto.digest.DigestUtil;
+
 import java.security.MessageDigest;
 
 public class Md5Learn {
 
-    public static void digest() {
-        String test = "test124345";
+    public static void digest(String content) {
+
         MessageDigest md5 = null;
         try {
             md5 = MessageDigest.getInstance("MD5");
         } catch (Exception e) {
             e.printStackTrace();
         }
-        byte[] digest = md5.digest(test.getBytes());
+        byte[] digest = md5.digest(content.getBytes());
         for (byte b : digest) {
             System.out.print(Integer.toHexString(b & 0xff));
         }
+        System.out.println();
+    }
+
+    public static void hutuDigest(String content) {
+        System.out.println(DigestUtil.md5Hex(content));
     }
 
     public static void main(String[] args) {
-        digest();
+
+        Long standardMatchId = 3805819L;
+        String dataSourceCode = "F01";
+        String standardCategoryId = "19";
+        String content = "Ronghe:StandardCategoryMarketData:" + standardMatchId + "_" + dataSourceCode + "_" +
+                standardCategoryId;
+        digest(content);
+        hutuDigest(content);
+
     }
 
-    
+
 }
--- syntax/src/main/java/kevin/project/number/NumberLearn.java
+++ syntax/src/main/java/kevin/project/number/NumberLearn.java
@@ -8,11 +8,28 @@ package kevin.project.number;
  **/
 public class NumberLearn {
 
+
+    public static void integerCompare() {
+        Integer a = 100;
+        Integer b = 100;
+        System.out.println(a == b);
+
+        a = 100000;
+        b = 100000;
+        System.out.println(a == b);
+
+
+        int c = 100000;
+        System.out.println(a == c);
+
+    }
+
     public static void xandLearn() {
         System.out.println(1 & 3);
     }
 
     public static void main(String[] args) {
         xandLearn();
+        integerCompare();
     }
 }
